378fec45ace872364d3d8a6b0ac74a2c90d67995
paysyak <paysyak@gmail.com>
2017-06-15 20:38:39 +0300

    Update pipe.js

--- pipe.js
+++ pipe.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
 const zlib = require('zlib');
 
+fs.readFile('text2.txt', (error, data) => {
+    console.log(data);
+});
+
+
 const readebleStream = fs.createReadStream('text.txt');
 const duplex = zlib.createGzip();
 const writeble = fs.createWriteStream('file.txt.gz');
 
 readebleStream.pipe(duplex).pipe(writeble);
+
